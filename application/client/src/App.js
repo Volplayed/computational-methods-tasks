c2e7067a53dcab093cbe650260f8374986c180bd
unknown <volplay3@gmail.com>
2023-09-25 20:44:40 +0300

    basic frontend get request

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  function getData() {
+    fetch('http://localhost:5000/newtons-method/?f=x**3-4%20-3&df=3*(x**2)&x=1&a=0&b=2&epsilon=1e-7')
+    .then(res => res.json()).then(data => {
+      console.log(data)
+        setData(data)})
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button className="newton" onClick={getData}>
+      Newton
+    </button>
+      <div className="data">
+      {
+      data.length === 0 ? (
+          <></>
+        ) : (
+          Object.values(data).map((x, i) => <p key={i}>{x}</p>)
+        )}
+      </div>
+    </div>   
   );
 }
+
 
 export default App;
--- application/flask-server/server.py
+++ application/flask-server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_file, request
-
+from flask_cors import CORS
 #methods
 from methods.Iteration.Newtons_method import newtons_method
 
@@ -7,10 +7,11 @@ from methods.Iteration.Newtons_method import newtons_method
 from methods.Iteration.tools import create_plot_iteration
 
 app = Flask(__name__)
-
+CORS(app)
 #Newton's method
 @app.route('/newtons-method/', methods=['GET'])
 def newtons_method_server():
+
     #get parameters
     f = request.args.get('f')
     df = request.args.get('df')
@@ -21,13 +22,14 @@ def newtons_method_server():
 
     #test url
     #http://localhost:5000/newtons-method/?f=x**3-4%20-3&df=3*(x**2)&x=1&a=0&b=2&epsilon=1e-7
-
-
-    return newtons_method(str(f), str(df), float(a), float(b), float(x0), float(epsilon))
+    print(newtons_method(str(f), str(df), float(a), float(b), float(x0), float(epsilon)))
+    response = newtons_method(str(f), str(df), float(a), float(b), float(x0), float(epsilon))
+    return response
 
 #plot Newton's method
 @app.route('/plot-newtons-method/', methods=['GET'])
 def plot_newtons_method_server():
+
     f = request.args.get('f')
     df = request.args.get('df')
     a = request.args.get('a')
@@ -42,7 +44,8 @@ def plot_newtons_method_server():
     #test url
     #http://localhost:5000/plot-newtons-method/?f=x**3-4%20-3&df=3*(x**2)&x_list=1.0,2.0,1.9166666666666667,1.9129384583070783,1.9129311828000604&a=0&b=2
 
-    return send_file(bytes_img, download_name='plot.png', mimetype='image/png')
+    response = send_file(bytes_img, download_name='plot.png', mimetype='image/png')
+    return response
 
 
 if __name__ == '__main__':
