cdae62ac52de2680756520f553a653dbfdf2ac88
unknown <volplay3@gmail.com>
2023-10-02 15:12:59 +0300

    disable active hud button

--- application/client/src/HUD.jsx
+++ application/client/src/HUD.jsx
@@ -8,7 +8,9 @@ export const HUD = ({state, onClick}) => {
                 {methods.map((method, i) => {
                     return (
                         <div key={i} className="HUD__item">
-                            <button className={(state === method) ? "HUD__button active" : "HUD__button"} onClick={() => onClick(method)}>{method}</button>
+                            <button className={(state === method) ? "HUD__button active" : "HUD__button"} 
+                            disabled={(state === method) ? true : false}
+                            onClick={() => onClick(method)}>{method}</button>
                         </div>    
                     )
                 })}
